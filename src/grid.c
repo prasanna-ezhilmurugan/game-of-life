b42093a6805dcccfdcaee69a9bb029f8335ebea0
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2025-03-04 17:24:26 +0530

    add grid udpate logic

--- src/grid.c
+++ src/grid.c
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
+void grid_print_matrix() {
+  for (int i = 0; i < grid_width; i++) {
+    for (int j = 0; j < grid_height; j++) {
+      printf("%d ", grid_matrix[i][j]);
+    }
+    printf("\n");
+  }
+}
+
 void grid_generate_random() {
   srand(time(0));
   for (int i = 0; i < grid_width; i++) {
     for (int j = 0; j < grid_height; j++) {
       grid_matrix[i][j] = rand() % 2;
-      printf("%d", grid_matrix[i][j]);
     }
-    printf("\n");
   }
+}
+
+void grid_load_matrix() {
+  grid_matrix[0][0] = 1;
+  grid_print_matrix();
 }
 
 void grid_render_matrix(SDL_Renderer *renderer) {
@@ -25,6 +37,37 @@ void grid_render_matrix(SDL_Renderer *renderer) {
         current_cell.x = grid_cell_size * i;
         current_cell.y = grid_cell_size * j;
         SDL_RenderFillRect(renderer, &current_cell);
+      }
+    }
+  }
+}
+
+void grid_matrix_update() {
+  for (int i = 0; i < grid_width; i++) {
+    for (int j = 0; j < grid_height; j++) {
+      int alive_neighbours = 0;
+      if (i - 1 >= 0 && grid_matrix[i - 1][j])
+        alive_neighbours++;
+      if (j - 1 >= 0 && grid_matrix[i][j - 1])
+        alive_neighbours++;
+      if (i - 1 >= 0 && j - 1 >= 0 && grid_matrix[i - 1][j - 1])
+        alive_neighbours++;
+      if (i + 1 < grid_width && grid_matrix[i + 1][j])
+        alive_neighbours++;
+      if (j + 1 < grid_height && grid_matrix[i][j + 1])
+        alive_neighbours++;
+      if (i + 1 < grid_width && j + 1 < grid_height &&
+          grid_matrix[i + 1][j + 1])
+        alive_neighbours++;
+      if (i - 1 >= 0 && j + 1 < grid_height && grid_matrix[i - 1][j + 1])
+        alive_neighbours++;
+      if (i + 1 < grid_width && j - 1 >= 0 && grid_matrix[i + 1][j - 1])
+        alive_neighbours++;
+
+      if (grid_matrix[i][j] && (alive_neighbours < 2 || alive_neighbours > 3)) {
+        grid_matrix[i][j] = 0;
+      } else if (!grid_matrix[i][j] && alive_neighbours == 3) {
+        grid_matrix[i][j] = 1;
       }
     }
   }
